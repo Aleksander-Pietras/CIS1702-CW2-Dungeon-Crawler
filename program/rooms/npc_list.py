eb35596b6035a35897e47bf6f12d0288092b79d3
Aleksander Pietras <alexpietras0@gmail.com>
2025-12-12 17:32:52 +0000

    The rooms and npcs

---
+++ program/rooms/npc_list.py
@@ -0,0 +1,11 @@
+npcs = {
+    "Shopkeeper Angus": {
+        "intro text": "Well what can I do for ya?",
+        # here we can put other stuff here like shop items
+    },
+    
+    "Informat Caelan": {
+        "name": "Caelan the Informat",
+        "into text": "Hello my frined, stay for a while and listen"
+    }
+}
