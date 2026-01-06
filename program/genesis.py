4beb4fccac7b6814a9c2970944114ed1092738aa
Aleksander Pietras <alexpietras0@gmail.com>
2026-01-06 13:43:53 +0000

    ADD f: intro_text()

--- program/genesis.py
+++ program/genesis.py
@@ -1,17 +1,8 @@
-from program.extra_usefull.path_finder import file_path_finder_all
-#from read_write_json.write_json import update_json
-
-from program.rooms.rooms import create_room
-from program.read_write_json.write_json import overwrite_json
-
-if __name__ == "__main__":
-    stop_creating=False
-    while stop_creating==False:
-        FILENAME = "database.json"
-        FILE_PATH = file_path_finder_all(FILENAME)
-        print(FILE_PATH)
-        room = create_room()
-        overwrite_json(room)
-        if input("Type 'stop' to finish creation. \n") == "stop":
-            stop_creating=True
-
+def intro_text():
+    print("-" * 40)
+    print("\n")
+    print("The great Dungeon Crawler Game")
+    print("Objective: survive as long as possible")
+    print("\n")
+    print("-" * 40)
+    print("")
